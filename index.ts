4ff39b8d3c82d2de5e472b8bb8e623fe2855b76e
ElMayo <mic.l.meier@gmail.com>
2020-05-09 07:35:18 +0200

    exercise 9.9 patientor backend step2

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
+const PORT = 3001;
+
 app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
-
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged endpoint /ping");
   res.send("pong");
 });
